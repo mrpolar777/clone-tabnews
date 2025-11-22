98e63fcfee79f5401b7301750f6870bcccca83c2
Henrique Costa <costa.henrique9577@gmail.com>
2025-11-22 17:38:22 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-	return <h1>Você é mais capaz do que imagina — e hoje é o dia perfeito para provar isso.🐇</h1>
+  return (
+    <h1>
+      Você é mais capaz do que imagina — e hoje é o dia perfeito para provar
+      isso.🐇
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
