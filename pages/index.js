501abeafaebe962b0fddcfe2075373b9d344ca9e
Henrique Costa <costa.henrique9577@gmail.com>
2025-11-22 14:36:40 +0000

    modificado a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-	return <h1>Você é mais capaz do que imagina — e hoje é o dia perfeito para provar isso.</h1>
+	return <h1>Você é mais capaz do que imagina — e hoje é o dia perfeito para provar isso.🐇</h1>
 }
 
 export default Home;
